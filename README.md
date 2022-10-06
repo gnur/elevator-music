7a52292ff8877866a9a9a8ee13ded23a30e23aea
Erwin de Keijzer <erwin@dekeijzer.xyz>
2022-10-06 14:05:49 +0200

    POC now plays actual music

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # elevator-music
 Project that will play music if an elevator is moving
+
+
+## attribtion
+
+The music in this project is [Zeit by 1st Contact](https://freemusicarchive.org/music/1st-contact/single/zeitmp3/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use soloud::*;
+
 fn main() {
     println!("Hello, world!");
+
+    let sl = Soloud::default().unwrap();
+    let mut wav = audio::Wav::default();
+    wav.load_mem(include_bytes!("../zeit.mp3")).unwrap();
+    sl.play(&wav);
+    while sl.voice_count() > 0 {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
 }
